e913cee1064034aceeed220426ec5e931500d091
T2 <t2.tide@gmail.com>
2017-04-10 02:47:51 +0900

    IgE検査項目を42種類に増やす

--- app/helpers/iges_helper.rb
+++ app/helpers/iges_helper.rb
@@ -1,25 +1,104 @@
 module IgesHelper
+  # アレルゲン項目
+  ALLERGEN_SORT = 
+    [
+      {"en" => 'housedust1', "ja" => 'ハウスダスト1'}, 
+      {"en" => 'housedust2', "ja" => 'ハウスダスト2'},
+      {"en" => 'yakehyoudani', "ja" => 'ヤケヒョウヒダニ'},
+      {"en" => 'sugi', "ja" => 'スギ'},
+      {"en" => 'hinoki', "ja" => 'ヒノキ'},
+      {"en" => 'hannoki', "ja" => 'ハンノキ'},
+      {"en" => 'shirakanba', "ja" => 'シラカンバ'},
+      {"en" => 'harugaya', "ja" => 'ハルガヤ'},
+      {"en" => 'kamogaya', "ja" => 'カモガヤ'},
+      {"en" => 'ooawagaeri', "ja" => 'オオアワガエリ'},
+      {"en" => 'butakusa', "ja" => 'ブタクサ'},
+      {"en" => 'yomogi', "ja" => 'ヨモギ'},
+      {"en" => 'alternaria', "ja" => 'アルテリナリア'},
+      {"en" => 'aspergillosis', "ja" => 'アスペルギルス'},
+      {"en" => 'candida', "ja" => 'カンジダ'},
+      {"en" => 'malassezia', "ja" => 'マラセチア'},
+      {"en" => 'neko', "ja" => 'ネコ（皮屑）'},
+      {"en" => 'inu', "ja" => 'イヌ（皮屑）'},
+      {"en" => 'kato', "ja" => '家兎上皮'},
+      {"en" => 'gokiburi', "ja" => 'ゴキブリ'},
+      {"en" => 'ga', "ja" => 'ガ'},
+      {"en" => 'gyunyu', "ja" => '牛乳'},
+      {"en" => 'ranpaku', "ja" => '卵白'},
+      {"en" => 'ovomucoid', "ja" => 'オボムコイド'},
+      {"en" => 'kome', "ja" => '米'},
+      {"en" => 'komugi', "ja" => '小麦（食品）'},
+      {"en" => 'soba', "ja" => 'ソバ'},
+      {"en" => 'daizu', "ja" => '大豆'},
+      {"en" => 'peanuts', "ja" => 'ピーナッツ'},
+      {"en" => 'ringo', "ja" => 'リンゴ'},
+      {"en" => 'banana', "ja" => 'バナナ'},
+      {"en" => 'kiwi', "ja" => 'キウイ'},
+      {"en" => 'goma', "ja" => 'ゴマ'},
+      {"en" => 'gyuniku', "ja" => '牛肉'},
+      {"en" => 'butaniku', "ja" => '豚肉'},
+      {"en" => 'toriniku', "ja" => '鶏肉'},
+      {"en" => 'ebi', "ja" => 'エビ'},
+      {"en" => 'kani', "ja" => 'カニ'},
+      {"en" => 'saba', "ja" => 'サバ'},
+      {"en" => 'sake', "ja" => 'サケ'},
+      {"en" => 'maguro', "ja" => 'マグロ'},
+      {"en" => 'latex', "ja" => 'ラテックス'}
+    ]
+
+  # 検査種類
+  TEST_CATEGORY =
+    [
+      "IgE(非特異的IgE)〔CLEIA〕",
+      "IgE(非特異的IgE)〔FEIA〕",
+      "特異的IgE(MAST36アレルゲン)",
+      "汎用セット スクリーニング16　内科・呼吸器系",
+      "汎用セット スクリーニング16　小児科",
+      "汎用セット スクリーニング16　耳鼻科",
+      "汎用セット スクリーニング16　皮膚科",
+      "汎用セット コンビネーション6",
+      "特異的IgE(ミックスアレルゲン)",
+      "特異的IgE(シングルアレルゲン)",
+      "アトピー鑑別試験 (12種吸入性アレルゲン)",
+      "特異的IgE (C-PAC16アレルゲン) 小児用",
+      "特異的IgE (C-PAC16アレルゲン) アトピー性皮膚炎用",
+      "特異的IgE (C-PAC16アレルゲン) 鼻炎・喘息用",
+      "特異的IgE (C-PAC5アレルゲン)　小児除去食用",
+      "特異的IgE(マルチアレルゲン)",
+      "特異的IgE(シングルアレルゲン)",
+      "特異的IgE(Ara h 2)(ピーナッツ由来)"
+    ]
+
+  # 検査単位
+  TEST_UNIT =
+    [
+      "IU/mL",
+      "UA/mL",
+      "IUA/mL",
+      "LC"
+    ]
+
   # 検査種類を表示する
-  def get_test_category_name(test_category_value)
+  def get_category_name(test_category_value)
     case test_category_value
-    when 0 then "IgE(非特異的IgE)〔CLEIA〕"
-    when 1 then	"IgE(非特異的IgE)〔FEIA〕"
-    when 2 then "特異的IgE(MAST36アレルゲン)"
-    when 3 then "汎用セット スクリーニング16　内科・呼吸器系"
-    when 4 then "汎用セット スクリーニング16　小児科"
-    when 5 then "汎用セット スクリーニング16　耳鼻科"
-    when 6 then "汎用セット スクリーニング16　皮膚科"
-    when 7 then "汎用セット コンビネーション6"
-    when 8 then "特異的IgE(ミックスアレルゲン) "
-    when 9 then "特異的IgE(シングルアレルゲン) "
-    when 10 then "アトピー鑑別試験 (12種吸入性アレルゲン) "
-    when 11 then "特異的IgE (C-PAC16アレルゲン) 小児用 "
-    when 12 then "特異的IgE (C-PAC16アレルゲン) アトピー性皮膚炎用 "
-    when 13 then "特異的IgE (C-PAC16アレルゲン) 鼻炎・喘息用 "
-    when 14 then "特異的IgE (C-PAC5アレルゲン)　小児除去食用 "
-    when 15 then "特異的IgE(マルチアレルゲン) "
-    when 16 then "特異的IgE(シングルアレルゲン) "
-    when 17 then "特異的IgE(Ara h 2)(ピーナッツ由来) "
+    when  0 then TEST_CATEGORY[0]
+    when  1 then TEST_CATEGORY[1]
+    when  2 then TEST_CATEGORY[2]
+    when  3 then TEST_CATEGORY[3]
+    when  4 then TEST_CATEGORY[4]
+    when  5 then TEST_CATEGORY[5]
+    when  6 then TEST_CATEGORY[6]
+    when  7 then TEST_CATEGORY[7]
+    when  8 then TEST_CATEGORY[8]
+    when  9 then TEST_CATEGORY[9]
+    when 10 then TEST_CATEGORY[10]
+    when 11 then TEST_CATEGORY[11]
+    when 12 then TEST_CATEGORY[12]
+    when 13 then TEST_CATEGORY[13]
+    when 14 then TEST_CATEGORY[14]
+    when 15 then TEST_CATEGORY[15]
+    when 16 then TEST_CATEGORY[16]
+    when 17 then TEST_CATEGORY[17]
     else
     end
   end
@@ -27,10 +106,10 @@ module IgesHelper
   # 単位を表示する
   def get_unit_name(unit_value)
     case unit_value
-    when 0 then "IU/mL"
-    when 1 then "UA/mL"
-    when 2 then "IUA/mL"
-    when 3 then "LC"
+    when 0 then TEST_UNIT[0]
+    when 1 then TEST_UNIT[1]
+    when 2 then TEST_UNIT[2]
+    when 3 then TEST_UNIT[3]
     else
     end
   end
--- db/migrate/20170402113409_create_iges.rb
+++ db/migrate/20170402113409_create_iges.rb
@@ -12,6 +12,9 @@ class CreateIges < ActiveRecord::Migration[5.0]
       t.decimal :allergen_housedust2_value
       t.integer :allergen_housedust2_unit
       t.integer :allergen_housedust2_class
+      t.decimal :allergen_yakehyoudani_value
+      t.integer :allergen_yakehyoudani_unit
+      t.integer :allergen_yakehyoudani_class 
       t.decimal :allergen_sugi_value
       t.integer :allergen_sugi_unit
       t.integer :allergen_sugi_class
@@ -24,6 +27,111 @@ class CreateIges < ActiveRecord::Migration[5.0]
       t.decimal :allergen_shirakanba_value
       t.integer :allergen_shirakanba_unit
       t.integer :allergen_shirakanba_class
+      t.decimal :allergen_harugaya_value
+      t.integer :allergen_harugaya_unit
+      t.integer :allergen_harugaya_class
+      t.decimal :allergen_kamogaya_value
+      t.integer :allergen_kamogaya_unit
+      t.integer :allergen_kamogaya_class
+      t.decimal :allergen_ooawagaeri_value
+      t.integer :allergen_ooawagaeri_unit
+      t.integer :allergen_ooawagaeri_class
+      t.decimal :allergen_butakusa_value
+      t.integer :allergen_butakusa_unit
+      t.integer :allergen_butakusa_class
+      t.decimal :allergen_yomogi_value
+      t.integer :allergen_yomogi_unit
+      t.integer :allergen_yomogi_class
+      t.decimal :allergen_alternaria_value
+      t.integer :allergen_alternaria_unit
+      t.integer :allergen_alternaria_class
+      t.decimal :allergen_aspergillosis_value
+      t.integer :allergen_aspergillosis_unit
+      t.integer :allergen_aspergillosis_class
+      t.decimal :allergen_candida_value
+      t.integer :allergen_candida_unit
+      t.integer :allergen_candida_class
+      t.decimal :allergen_malassezia_value
+      t.integer :allergen_malassezia_unit
+      t.integer :allergen_malassezia_class
+      t.decimal :allergen_neko_value
+      t.integer :allergen_neko_unit
+      t.integer :allergen_neko_class
+      t.decimal :allergen_inu_value
+      t.integer :allergen_inu_unit
+      t.integer :allergen_inu_class
+      t.decimal :allergen_kato_value # 家兎
+      t.integer :allergen_kato_unit
+      t.integer :allergen_kato_class
+      t.decimal :allergen_gokiburi_value
+      t.integer :allergen_gokiburi_unit
+      t.integer :allergen_gokiburi_class
+      t.decimal :allergen_ga_value
+      t.integer :allergen_ga_unit
+      t.integer :allergen_ga_class
+      t.decimal :allergen_gyunyu_value
+      t.integer :allergen_gyunyu_unit
+      t.integer :allergen_gyunyu_class
+      t.decimal :allergen_ranpaku_value
+      t.integer :allergen_ranpaku_unit
+      t.integer :allergen_ranpaku_class
+      t.decimal :allergen_ovomucoid_value
+      t.integer :allergen_ovomucoid_unit
+      t.integer :allergen_ovomucoid_class
+      t.decimal :allergen_kome_value
+      t.integer :allergen_kome_unit
+      t.integer :allergen_kome_class
+      t.decimal :allergen_komugi_value
+      t.integer :allergen_komugi_unit
+      t.integer :allergen_komugi_class
+      t.decimal :allergen_soba_value
+      t.integer :allergen_soba_unit
+      t.integer :allergen_soba_class
+      t.decimal :allergen_daizu_value
+      t.integer :allergen_daizu_unit
+      t.integer :allergen_daizu_class
+      t.decimal :allergen_peanuts_value
+      t.integer :allergen_peanuts_unit
+      t.integer :allergen_peanuts_class
+      t.decimal :allergen_ringo_value
+      t.integer :allergen_ringo_unit
+      t.integer :allergen_ringo_class
+      t.decimal :allergen_banana_value
+      t.integer :allergen_banana_unit
+      t.integer :allergen_banana_class
+      t.decimal :allergen_kiwi_value
+      t.integer :allergen_kiwi_unit
+      t.integer :allergen_kiwi_class
+      t.decimal :allergen_goma_value
+      t.integer :allergen_goma_unit
+      t.integer :allergen_goma_class
+      t.decimal :allergen_gyuniku_value
+      t.integer :allergen_gyuniku_unit
+      t.integer :allergen_gyuniku_class
+      t.decimal :allergen_butaniku_value
+      t.integer :allergen_butaniku_unit
+      t.integer :allergen_butaniku_class
+      t.decimal :allergen_toriniku_value
+      t.integer :allergen_toriniku_unit
+      t.integer :allergen_toriniku_class
+      t.decimal :allergen_ebi_value
+      t.integer :allergen_ebi_unit
+      t.integer :allergen_ebi_class
+      t.decimal :allergen_kani_value
+      t.integer :allergen_kani_unit
+      t.integer :allergen_kani_class
+      t.decimal :allergen_saba_value
+      t.integer :allergen_saba_unit
+      t.integer :allergen_saba_class
+      t.decimal :allergen_sake_value
+      t.integer :allergen_sake_unit
+      t.integer :allergen_sake_class
+      t.decimal :allergen_maguro_value
+      t.integer :allergen_maguro_unit
+      t.integer :allergen_maguro_class
+      t.decimal :allergen_latex_value
+      t.integer :allergen_latex_unit
+      t.integer :allergen_latex_class
 
       t.timestamps
     end
