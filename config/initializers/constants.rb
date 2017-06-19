cb9201f7ffb1b200e20f80564a6dd5fe261e46bc
T2 <t2.tide@gmail.com>
2017-06-19 23:36:35 +0900

    #13 タイトルやアイキャッチを変更

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 module Constants
     ## Constants::HOGEでアクセスできる
-    SITE_TITLE = 'Allergy SNS'
+    SITE_TITLE = 'Allergy LOG'
     DIAGNOSISCHECK_URL = 'http://check.allergy.blue/test'
     ALLERGYBLUE_URL = 'http://allergy.blue'
 end
