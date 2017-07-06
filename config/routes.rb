ae836e1e26de57b56953f2b92078540c819baaaf
T2 <t2.tide@gmail.com>
2017-07-07 03:57:09 +0900

    move search routing to collection block

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
 
   root 'static_pages#home'
+
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  get    '/home_logined',   to: 'users#home_logined'
+  get    '/home',   to: 'users#home_logined'
+
   resources :users do
+    collection do
+      get 'search_by_ige'
+      post 'search_result_by_ige'
+      get 'search_by_allergen'
+      get 'search_result_by_allergen'
+      get 'search_by_name'
+      post 'search_result_by_name'
+    end
     member do
       get :following, :followers
     end
@@ -28,10 +38,4 @@ Rails.application.routes.draw do
       get 'quote'
     end
   end
-  get '/search_by_ige', to: 'users#search_by_ige'
-  post '/search_result_by_ige', to: 'users#search_result_by_ige'
-  get '/search_by_allergen', to: 'users#search_by_allergen'
-  get '/search_result_by_allergen', to: 'users#search_result_by_allergen'
-  get '/search_by_name', to: 'users#search_by_name'
-  post '/search_result_by_name', to: 'users#search_result_by_name'
 end
