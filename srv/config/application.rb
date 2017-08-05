49946e79a9eb96fc6af5912df59f8b535fd7f2d6
T2 <t2.tide@gmail.com>
2017-08-05 16:24:08 +0900

    chore(srv.config): not generate controller test

--- srv/config/application.rb
+++ srv/config/application.rb
@@ -29,5 +29,10 @@ module Srv
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec,
+        controller_specs: false
+    end
+
   end
 end
