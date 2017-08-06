01ece855eeb6eb93fa9687043c1a8940837ff7fc
T2 <t2.tide@gmail.com>
2017-08-06 11:51:31 +0900

    chore(srv): config CORS with all

--- srv/config/initializers/cors.rb
+++ srv/config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins '*'
 
-    resource '*',
+    resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
