070de985fece8adf2f0b31fffcf7241a8f0461ac
T2 <t2.tide@gmail.com>
2017-08-25 07:33:45 +0900

    chore(all.deploy): deploy to heroku from circleCI

--- build.sh
+++ build.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
+HOME=`pwd`
+
+echo $HOME
+
 clear() {
-    cd ./srv/public/ && \
+    cd ./srv/public && \
     ls | grep -v -E 'robots.txt' | xargs rm -r && \
-    cd ../../
+    cd $HOME
 }
 
 cd ./cli
 
 echo 'Do Ahead of Time compilation.'
-ng build --prod
+npm run build:prod
 [ $? -ne 0 ] && exit 1
 
-cd ..
+cd $HOME
 
 echo 'Move Static Files to Public folder.'
 clear && \
-cp -r ./cli/dist/ ./srv/public/
+cp -r ./cli/dist/* ./srv/public/
 [ $? -ne 0 ] && exit 1
 
 exit 0
