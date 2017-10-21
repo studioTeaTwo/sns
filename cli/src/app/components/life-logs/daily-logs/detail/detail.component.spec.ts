47f09ca45ea249d69e04d1c5af4251b1e3bb7b3a
T2 <t2.tide@gmail.com>
2017-10-22 06:11:12 +0900

    fix(cli.daily-log): test

--- cli/src/app/components/life-logs/daily-logs/detail/detail.component.spec.ts
+++ cli/src/app/components/life-logs/daily-logs/detail/detail.component.spec.ts
@@ -1,9 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { DetailComponent } from './detail.component';
 
+import { SharedModule } from 'app/shared/shared.module';
 import { Store } from 'app/shared/store/store';
 import { DailyLogService } from 'app/shared/services/api';
 
@@ -16,6 +19,9 @@ describe('DetailComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
+        FormsModule,
+        NoopAnimationsModule,
+        SharedModule,
       ],
       declarations: [ DetailComponent ],
       providers: [
