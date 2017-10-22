1193ffbfbfe84bccfc9cdd59f1c85315438d38d2
T2 <t2.tide@gmail.com>
2017-10-22 21:51:15 +0900

    feat(srv.routing): route to root when SPA's url is requested

--- srv/config/routes.rb
+++ srv/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#index'
+  get '*path', to: 'static_pages#index'
 
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
