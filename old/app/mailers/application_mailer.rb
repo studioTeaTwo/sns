03c8c323ff2ece2236b438ef429acc2162b1b915
T2 <t2.tide@gmail.com>
2017-10-29 00:04:43 +0900

    chore(srv) remove old app

--- old/app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@allergy.blue'
-  layout 'mailer'
-end
--- old/config/initializers/constants.rb
+++
@@ -1,6 +0,0 @@
-module Constants
-    ## Constants::HOGEでアクセスできる
-    SITE_TITLE = 'Allergy LOG'
-    DIAGNOSISCHECK_URL = 'http://check.allergy.blue/test'
-    ALLERGYBLUE_URL = 'http://allergy.blue'
-end
