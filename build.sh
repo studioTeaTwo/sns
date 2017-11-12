f305847e2dae3e3aa3d08172d6c3e4dbe7b96223
T2 <t2.tide@gmail.com>
2017-11-12 21:23:18 +0900

    chore(all): build script support JIT build

--- build.sh
+++ build.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-HOME=`pwd`
-
-echo $HOME
-
 clear() {
     cd ./srv/public && \
     ls | grep -v -E 'robots.txt' | xargs rm -rff && \
     cd $HOME
 }
 
+HOME=`pwd`
+
+echo $HOME
+
 cd ./cli
 
-echo 'Do Ahead of Time compilation.'
-npm run build:prod
+# ビルド実行
+
+if [ $1 = dev ]; then
+  echo 'Do Just in Time compilation.'
+  npm run build
+else
+  echo 'Do Ahead of Time compilation.'
+  npm run build:prod
+fi
 [ $? -ne 0 ] && exit 1
+
+# ファイル配置
 
 cd $HOME
 
