d6ad1a9202896a48a1540bbe411ac641c9008291
T2 <t2.tide@gmail.com>
2018-01-04 01:34:25 +0900

    feat(cli.pwa): implement service worker

--- cli/src/app/app.module.ts
+++ cli/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from 'environments/environment';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +27,7 @@ import { ContactComponent } from './components/navigations/contact/contact.compo
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     SharedModule.forRoot(),
     AppRoutingModule,
   ],
