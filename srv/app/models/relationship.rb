c4a48718bf2976dcbc3bc34df71343c8628612e2
T2 <t2.tide@gmail.com>
2018-02-14 17:09:37 +0900

    fix(srv.relationship): detroy experience together because it can't resolve follower

--- srv/app/models/relationship.rb
+++ srv/app/models/relationship.rb
@@ -1,7 +1,7 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-  has_one :experience, as: :activity # experienceはdestroyしない
+  has_one :experience, as: :activity, dependent: :destroy
   has_one :notification, as: :activity, dependent: :destroy
 
   validates :follower_id, presence: true
