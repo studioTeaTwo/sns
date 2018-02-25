025cc670d7535160c2458c06dc76643567d4c4da
T2 <t2.tide@gmail.com>
2018-02-25 13:54:18 +0900

    feat(cli.top): move automatically home when logging in

--- cli/src/app/app.component.spec.ts
+++ cli/src/app/app.component.spec.ts
@@ -2,14 +2,19 @@ import { TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SpyLocation } from '@angular/common/testing';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AppComponent } from './app.component';
 
 import { MaterialModule } from 'app/shared/material/material.module';
 import { Store } from 'app/core/store/store';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/navigations/header/header.component';
-import { GlobalMenuComponent } from './components/navigations/global-menu/global-menu.component';
 import { AccountService } from 'app/core/services/api';
 import { MockAccountService } from 'testing/api';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/navigations/header/header.component';
+import { GlobalMenuComponent } from './components/navigations/global-menu/global-menu.component';
+import { TopComponent } from './components/navigations/top/top.component';
+import { ContactComponent } from './components/navigations/contact/contact.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -18,11 +23,14 @@ describe('AppComponent', () => {
         HttpClientTestingModule,
         RouterTestingModule,
         MaterialModule,
+        AppRoutingModule,
       ],
       declarations: [
         AppComponent,
         HeaderComponent,
         GlobalMenuComponent,
+        TopComponent,
+        ContactComponent,
       ],
       providers: [
         Store,
@@ -30,6 +38,10 @@ describe('AppComponent', () => {
           provide: AccountService,
           useClass: MockAccountService
         },
+        {
+          provide: APP_BASE_HREF,
+          useValue: '/'
+        }
       ]
     }).compileComponents();
   }));
--- cli/src/app/app.component.ts
+++ cli/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-import { UrlSerializer } from '@angular/router';
+import { UrlSerializer, Router } from '@angular/router';
 
 import { Store } from 'app/core/store/store';
+import { AccountService } from 'app/core/services/api';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +18,26 @@ export class AppComponent {
 
   constructor(
     private location: Location,
+    private router: Router,
     private urlSerializer: UrlSerializer,
     private store: Store,
+    private accountService: AccountService,
   ) {
     this.height = window.innerHeight - 42 - 50; // 42 = header.height 50 = footer.height
+
+    // index.htmlの初回ロード用タグを消去する
     const initialElm = document.getElementById('initial-paint') as HTMLDivElement;
     if (initialElm) { initialElm.remove(); }
+
+    // ログインしてたらホームに移動する
+    if (this.location.path() === '') {
+      this.accountService.isLoggedIn()
+        .then((isLoggedIn: boolean) => {
+          if (isLoggedIn) {
+            this.router.navigateByUrl('/home');
+          }
+        });
+    }
 
     // フロントのURLでサーバに飛んだ時の対応
     if (this.location.path().match(/url/)) {
